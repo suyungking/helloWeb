a4455576eae123c0154c9d72f182ea0adc53e1ee
suyungking <105546111+suyungking@users.noreply.github.com>
2025-06-11 11:13:52 +0900

    first commit

---
+++ src/main/java/org/kosa/hello/HomeController.java
@@ -0,0 +1,19 @@
+package org.kosa.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.ServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index(ServletRequest request) {
+		log.info("server port = {}" , request.getServerPort());
+		return "Hello world... " + request.getServerPort();
+	}
+
+}
