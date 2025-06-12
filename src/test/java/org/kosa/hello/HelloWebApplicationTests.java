f4144f4e025c5d05bf610f6cfb54634eead9bc6d
suyungking <105546111+suyungking@users.noreply.github.com>
2025-06-12 12:12:10 +0900

    테스트 추가

--- src/test/java/org/kosa/hello/HelloWebApplicationTests.java
+++ src/test/java/org/kosa/hello/HelloWebApplicationTests.java
@@ -1,5 +1,7 @@
 package org.kosa.hello;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,19 @@ class HelloWebApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	private int add(int a, int b) {
+		return a+b * 10;
+	}
+	@Test
+	void testAdd() {
+		int a = 10;
+		int b = 20;
+		int sum = add(a,b);
+		
+		assertEquals(30, sum);
+
 	}
 
 }
